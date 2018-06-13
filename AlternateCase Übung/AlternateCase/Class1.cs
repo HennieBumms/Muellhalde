7130152ac7a57dabdc20e19e5d5edb9437bdd2f0
Hendrik Zabel <hendrikzabel@gmail.com>
2018-06-13 14:24:52 +0200

    gfj

--- AlternateCase Übung/AlternateCase/Class1.cs
+++ AlternateCase Übung/AlternateCase/Class1.cs
@@ -1,9 +1,26 @@
 ﻿using System;
+using System.IO;
 
 namespace AlternateCase
 {
-    public class AlternateCaseWriter
-    {
+    public class AlternateCaseWriter : StreamWriter {
+        private MemoryStream memoryStream;
+
+        public AlternateCaseWriter(Stream stream) : base(stream) {
+            memoryStream = (MemoryStream)stream;
+        }
+
+        public void Write(string value) {
+            String bla = "";
+            foreach (var c in value) {
+                if (Char.IsLower(c)) {
+                    bla += Char.ToUpper(c);
+                } else {
+                    bla += Char.ToLower(c);
+                }
+            }
+            base.Write(bla);
+        }
 
     }
 }
